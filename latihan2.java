1185129a175e2bf14e02e3fc320c5911550542d3
21343001-adelinasuciko <112172809+21343001-adelinasuciko@users.noreply.github.com>
2022-09-15 09:49:54 +0700

    Add files via upload

---
+++ latihan2.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @author 21343011_Adelina Suciko
+ */
+package JS4Adelina;
+
+public class latihan2 {
+
+	public static void main(String[] args) {
+		int score = 0;
+		char answer = 'a';
+		
+		score = (answer == 'b') ? 10 : 0 ;
+		System.out.println("Score = " + score);
+
+	}
+
+}
---
+++ latihan5.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ * @author 21343011_Adelina Suciko
+ */
+package JS4Adelina;
+
+public class latihan5 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		int j = 0;
+		boolean test = true;
+		
+		//demonstrasi &&
+		test = (i > j)&&(j++ >9);
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(test);
+		
+		//demonstrasi &
+		test = (i > j)&(j++ >9);
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(test);
+
+	}
+
+}
